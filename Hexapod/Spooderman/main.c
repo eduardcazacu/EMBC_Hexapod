69e8163c30bc7f9736c03f29ccf398bd69187111
Tijmen van Nisselrooij <tijmen.van.nisselrooij@outlook.com>
2019-06-16 16:46:25 +0200

    Update main.c
    Simple case statement for walking and some basic layout how main will have to look

--- Hexapod/Spooderman/main.c
+++ Hexapod/Spooderman/main.c
@@ -7,6 +7,7 @@
 #include <sys/attribs.h>
 #include "config.h"
 
+enum WState{fwd,bwk,lft,rgt,stp};
 
 
 #define baud 115200
@@ -14,13 +15,39 @@ int freq = 40000000;
 
 void main() {
     //__builtin_enable_interrupts();
+    
+    enum WState Walkstate;
+    Walkstate = stp;
     macro_enable_interrupts();
     UART_Init(baud);
     UART_InitPoll(baud);
     sequencer_init(freq);
-    
+    DelayAprox10Us(1000); //Wait 100ms for initialization
     sequencer_wake();
-    //sequencer_walkForward();
-    sequencer_walkBack();
-    while(1);
+    DelayAprox10Us(1000); //Wait 100ms for Spooder to stand up
+    
+    
+    
+    
+    while(1)    //While only when connected to BT, to be implemented (Watchdog?)
+    {
+        //HERE: command decoder. Split walk commands from scan commands?
+        switch(WalkState)
+        {
+        case stp:
+            sequencer_stop();
+        case fwd:
+            sequencer_walkForward();
+        case bwk:
+            sequencer_walkBack();
+        case lft:
+            sequencer_walkLeft();
+        case rgt:
+            sequencer_walkRight();
+        default:
+            sequencer_stop();
+        }
+        
+        //HERE: Scan switch case?
+    }
 }
